a9671f2d1b094fd75001b278b416c9f8155afefe
Paluch77 <58185659+Paluch77@users.noreply.github.com>
2022-10-16 13:26:03 +0200

    1

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "main-background": "url('/data/background.jpg')",
+        "gem-diamond": "url('/data/diamond.png')",
+        earth: "url('/data/planet-earth.png')",
+        laptop: "url('/data/laptop.png')",
+        heart: "url('/data/heart.png')",
+      },
+    },
+  },
+  plugins: [],
+};
